e83362345310b4d2b7d07535e9cc94da3d0e5457
blueskyawen <bluesky.liu@163.com>
2019-03-01 00:52:38 +0800

    add silide

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng-trip-aibiying';
   contents : number[] = [1,2,3,4,5,6];
+  slideOptions : any[] = [
+    {url: 'https://z1.muscache.cn/pictures/00b12c64-0851-40e7-83aa-6bba10221435.jpg',
+      disable: false,
+      callback: () => {console.log('callback1');}},
+    {url: 'https://z1.muscache.cn/pictures/9bd8f08c-2e78-4665-ab7b-e4952673dabd.jpg',
+      disable: false,
+      callback: () => {console.log('callback2');}},
+    {url: 'https://z1.muscache.cn/pictures/30c10f87-af4b-45e5-8b6d-1a95c88fd5dd.jpg',
+      disable: false,
+      callback: () => {console.log('callback3');}}
+  ];
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+  }
+
 }
