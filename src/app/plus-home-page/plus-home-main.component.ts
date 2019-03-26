255d07ac599e59142740106f71714421243c1db9
blueskyawen <bluesky.liu@163.com>
2019-03-27 00:09:51 +0800

    add plus

--- src/app/plus-home-page/plus-home-main.component.ts
+++ src/app/plus-home-page/plus-home-main.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./plus-home-main.component.less']
 })
 export class PlusHomeMainComponent implements OnInit {
-
+  showLoading: boolean = false;
+  plusImg: string = 'https://z1.muscache.cn/4ea/air/v2/pictures/fb7d63bb-9220-467e-8265-132438393ee3.jpg?t=c:w3393-h1029,r:w3393-h1029-sfit,e:fjpg-c75';
   constructor() { }
 
   ngOnInit() {
