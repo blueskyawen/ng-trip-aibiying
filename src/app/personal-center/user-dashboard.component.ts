6fc879e2fa463c906099e8ce9829f83f0b8f4eac
liuxuwen10140721 <liu.xuwen@zte.com.cn>
2019-04-28 18:47:19 +0800

    add user message

--- src/app/personal-center/user-dashboard.component.ts
+++ src/app/personal-center/user-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PageRegisterLoginService } from '../home-page/page-register-login/page-register-login.service';
 
 @Component({
   selector: 'app-user-dashboard',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserDashboardComponent implements OnInit {
   showLoading: boolean = false;
-  constructor() { }
+  showDbHead: boolean = true;
+  userData: any;
+  userImg: string = 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1551707882841&di=66ee26cdc981feac8194e697a58e220e&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fblog%2F201409%2F02%2F20140902122803_cjEEi.thumb.700_0.jpeg';
+  messages: any[] = [];
+  constructor(public pRLoginService: PageRegisterLoginService) { }
 
   ngOnInit() {
     this.showLoading = true;
--- src/app/personal-center/user-info-box.component.ts
+++ src/app/personal-center/user-info-box.component.ts
@@ -7,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserInfoBoxComponent implements OnInit {
   showLoading: boolean = false;
+  infoOptions: any[] = [
+    {label: '全部消息(0)',value: 'all',disable: false},
+    {label: '未读(0)',value: 'unread',disable: false},
+    {label: '待处理(0)',value: 'pending',disable: false},
+    {label: '已归档(0)',value: 'archived',disable: false}
+  ];
+  infoData: any = {
+    all: [],
+    unread: [],
+    pending: [],
+    archived: []
+  };
+  selectedInfo: string = 'all';
+  showInfoList: any[] = [];
   constructor() { }
 
   ngOnInit() {
+    this.infoOptions[0].label = `全部消息(${this.infoData.unread.length+this.infoData.pending.length+this.infoData.archived.length})`;
+    this.infoOptions[1].label = `未读(${this.infoData.unread.length})`;
+    this.infoOptions[2].label = `待处理(${this.infoData.pending.length})`;
+    this.infoOptions[3].label = `已归档(${this.infoData.archived.length})`;
     this.showLoading = true;
-    setTimeout(() => {this.showLoading = false;}, 2000);
+    setTimeout(() => {this.showLoading = false;}, 1000);
+  }
+
+  labelChange() {
+    this.showInfoList = this.infoData[this.selectedInfo];
   }
 }
