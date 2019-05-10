5d946d310dea94bc40e4057ee55410d2755d09da
liuxuwen10140721 <liu.xuwen@zte.com.cn>
2019-05-10 19:56:26 +0800

    add user notify

--- src/app/personal-center/user-data/user-notify.component.ts
+++ src/app/personal-center/user-data/user-notify.component.ts
@@ -6,10 +6,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-notify.component.less']
 })
 export class UserNotifyComponent implements OnInit {
-
+  userInfo : any = {
+    message: {
+      email: true, msgPush: false, phoneMsg: false
+    },
+    contact: {
+      email: '******@163.com ', phoneMsg: '+86 *** **** 0560'
+    },
+    remind: {
+      email: true, msgPush: true, phoneMsg: true
+    },
+    promote: {
+      email: true, msgPush: true, phoneMsg: true, phone: false
+    },
+    pocity: {
+      email: true, msgPush: false, phoneMsg: false, phone: false
+    },
+    support: {
+      email: false, msgPush: false, phoneMsg: false
+    },
+    phoneMsg: ['+86 *** **** 0560']
+  };
+  labels: any = {
+    email: '电子邮件', msgPush: '推送通知', phoneMsg: '短信', phone: '手机号'
+  };
+  userInfoShow: any = {
+    message: [], remind: [], promote: [], pocity: [], support: []
+  };
   constructor() { }
 
   ngOnInit() {
+    for(let key in this.userInfo) {
+      if(key !== 'phoneMsg' && key !== 'contact') {
+        for(let key2 in this.userInfo[key]) {
+          this.userInfoShow[key].push({
+            label: this.labels[key2],
+            checked: this.userInfo[key][key2] ? 1 : 0,
+            Disable: false
+          });
+        }
+      }
+    }
+    console.log(this.userInfoShow);
   }
 
 }
