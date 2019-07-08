1fd95dd7ab9963368b5833b2ca7255634c7488ae
liuxuwen10140721 <liu.xuwen@zte.com.cn>
2019-07-08 20:56:40 +0800

    fix for xs 05

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router }  from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,11 +8,14 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'ng-trip-aibiying';
-
-  constructor() {
+  constructor(private router: Router) {
 
   }
 
   ngOnInit() {
+    this.router.events.subscribe(event => {
+      // @ts-ignore
+      window.pageYOffset = 0;
+    });
   }
 }
