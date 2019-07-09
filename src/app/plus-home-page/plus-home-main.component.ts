1dcff70304090abdef69848dc810fab4e77c2796
liuxuwen10140721 <liu.xuwen@zte.com.cn>
2019-07-09 20:25:24 +0800

    fix for xs 08

--- src/app/plus-home-page/plus-home-main.component.ts
+++ src/app/plus-home-page/plus-home-main.component.ts
@@ -10,9 +10,15 @@ export class PlusHomeMainComponent implements OnInit {
   showLoading: boolean = false;
   plusImg: string = 'https://z1.muscache.cn/4ea/air/v2/pictures/fb7d63bb-9220-467e-8265-132438393ee3.jpg?t=c:w3393-h1029,r:w3393-h1029-sfit,e:fjpg-c75';
 
-  constructor(public plusPageService: PlusPageService) { }
+  constructor(public plusPageService: PlusPageService) {
+
+  }
 
   ngOnInit() {
+    this.showLoading = true;
+    setTimeout(() => {
+      this.showLoading = false;
+    }, 500)
   }
 
 }
